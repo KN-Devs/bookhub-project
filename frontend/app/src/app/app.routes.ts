75ab17943effe1b0412fc8735ab305c80f9ffd02
maxime <maxime.lobjois2025@campus-eni.fr>
2026-04-15 14:30:58 +0200

    test

--- frontend/app/src/app/app.routes.ts
+++ frontend/app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {ConnectionView} from './View/connection-view/connection-view';
+import {AllBooksView} from './View/all-books-view/all-books-view';
+import {RegisterView} from './View/register-view/register-view';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ConnectionView },
+  { path: 'viewBooks', component: AllBooksView },
+  { path: 'register', component: RegisterView },
+];
