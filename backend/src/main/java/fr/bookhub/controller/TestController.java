5fe9a59a3de09f714ecadf8693c3cc5dc7566a80
KN-Dev <vincent.devault1@gmail.com>
2026-04-15 11:27:05 +0200

    Initialisation du projet ... Pret !!!

--- backend/src/main/java/fr/bookhub/controller/TestController.java
+++ backend/src/main/java/fr/bookhub/controller/TestController.java
@@ -1,13 +1,14 @@
 package fr.bookhub.controller;
 
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class TestController {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public String test() {
         return "OK";
     }
